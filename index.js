57419bd3ad628052ee55f0c093a34906ef10e39e
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-30 11:39:39 +0000

    API created to update user info in server and DB

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-/* 
-user: mostafiz1
-pass: OOFArUk03SmK0o8r
-*/
-
-
-
 const uri = "mongodb+srv://mostafiz1:OOFArUk03SmK0o8r@cluster0.mniec4l.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -32,6 +25,14 @@ async function run() {
             res.send(users);
         })
 
+        /* receive data from client side and send to DB */
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+            console.log(result);
+        })
+
         /* update specific user info */
         app.get('/users/:id', async (req, res) => {
             const id = req.params.id;
@@ -40,12 +41,22 @@ async function run() {
             res.send(user);
         })
 
-        /* receive data from client side and send to DB */
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            const result = await userCollection.insertOne(user);
+        /* update user in DB and server */
+        app.put('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const updatedUser = req.body;
+            const option = { upsert: true };
+            const newUpdatedUser = {
+                $set: {
+                    name: updatedUser.name,
+                    address: updatedUser.address,
+                    email: updatedUser.email
+                }
+            };
+            const result = await userCollection.updateOne(filter, newUpdatedUser, option);
             res.send(result);
-            console.log(result);
+            console.log(updatedUser)
         })
 
         /* delete data from DB and server */
